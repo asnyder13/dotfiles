18faff0748a6622d944e2117b0e760671025e85b
Andrew Snyder <snyder13@vt.edu>
2021-05-19 18:04:53 -0400

    feat: Use Paq for neovim package manager.  Only use init.lua (no source vimrc).

--- init.lua
+++ init.lua
@@ -3,10 +3,119 @@ local fn = vim.fn
 local g = vim.g
 local api = vim.api
 
+cmd('packadd paq-nvim')
+local paq = require'paq-nvim'.paq
+paq{'savq/paq-nvim', opt=true}
+
+-- Regular vim
+paq 'ntpeters/vim-better-whitespace'
+paq 'tpope/vim-commentary'
+paq 'airblade/vim-gitgutter'
+paq 'machakann/vim-highlightedyank'
+paq 'sheerun/vim-polyglot'
+paq 'vim-scripts/ReplaceWithRegister'
+paq 'ngmy/vim-rubocop'
+paq 'justinmk/vim-sneak'
+paq 'kshenoy/vim-signature'
+paq 'tpope/vim-surround'
+paq 'tpope/vim-vinegar'
+paq 'crusoexia/vim-monokai'
+
+-- Neovim specific
+paq 'norcalli/nvim-colorizer.lua'
+paq 'ojroques/nvim-hardline'
+paq 'phaazon/hop.nvim'
+paq 'nvim-lua/plenary.nvim'
+paq 'nvim-lua/popup.nvim'
+paq 'nvim-telescope/telescope.nvim'
+
+-- Old vimrc
 cmd([[
-	set runtimepath^=~/.vim runtimepath+=~/.vim/after
-	let &packpath = &runtimepath
-	source ~/.vim/vimrc
+	autocmd BufNewFile,BufRead *.npmrc   set syntax=dosini
+	autocmd BufNewFile,BufRead *bash-fc* set syntax=sh
+
+	" General config
+	set clipboard=unnamed
+	set number
+	set cursorline
+	set tabstop=2
+	set shiftwidth=2
+	set noexpandtab
+	set autoindent
+	set showmatch
+	set visualbell
+	set showmode
+	set wildmode=list:longest,longest:full
+	set scrolloff=0
+
+	set confirm
+	set nobackup
+	set viminfo='20,\"500
+	set hidden
+	set history=1000
+	set termguicolors
+
+	autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+
+	" https://github.com/jeffkreeftmeijer/vim-numbertoggle
+	augroup NumberToggle
+		autocmd!
+		autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+		autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+	augroup END
+
+	" Searching
+	set hlsearch
+	set ignorecase
+	set smartcase
+
+	" Folding
+	set foldmethod=syntax
+	set foldlevelstart=99
+
+	" Syntax hl/colors
+	syntax on
+	colorscheme monokai
+	let g:monokai_term_italic = 1
+	let g:monokai_gui_italic = 1
+	" Highlight tab indents
+	set list lcs=tab:\|\ 
+
+	" Remaps
+	"  Quick Easymotion
+	map <Leader> <Plug>(easymotion-prefix)
+	"  Collapse all levels under current fold
+	nnoremap zs zCzozo
+	"  Quick buffer switch (for tabline)
+	nnoremap gbn :bn<CR>
+	nnoremap gbN :bN<CR>
+	nnoremap gbd :bd<CR>
+	"  Quick buffer list
+	nnoremap <leader>b :ls<CR>:b
+
+	" netrw
+	let g:netrw_banner = 0
+	let g:netrw_liststyle = 3
+	let g:netrw_browse_split = 4
+	let g:netrw_altv = 1
+	let g:netrw_winsize = 25
+
+	""""""" Plugin settings
+	" Sneak
+	let g:sneak#use_ic_scs = 1
+	let g:sneak#map_netrw = 1
+
+	" Highlighted yank
+	let g:highlightedyank_highlight_duration = 500
+
+	" Polyglot
+	let g:ruby_recommended_style = 0
+
+	" Signature
+	let g:SignatureMarkTextHLDynamic = 1
+
+	" Rubocop
+	nmap <Leader>r :RuboCop <CR>
 ]])
 
 cmd([[
--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ link_dotfile () {
 		printf "%s is already linked\n" "$linkfile"
 		return 0
 	elif [[ ! -L $linkfile && -f $linkfile ]]; then
-		mkdir -p "$linkpath"
 		printf "%s renamed to %s-bkup\n" "$linkfile" "$linkfile"
 		cp "$linkfile" "$linkfile-bkup"
 	fi
 
+	mkdir -p "$linkpath"
 	ln -sfv "$scriptpath/$dotfile" "$linkfile"
 }
 
@@ -32,99 +32,89 @@ for ((i = 0; i < ${#DOTFILES[@]}; i++)); do
 	link_dotfile ${DOTFILES[i]} ${DOTFILE_LINKS[i]}
 done
 
-### Vim config
-# Vim theme
-if command -v wget >/dev/null 2>&1;   then fetcher='wget -O'
-elif command -v curl >/dev/null 2>&1; then fetcher='curl -o'
-fi
-
-mkdir -p ~/.vim/colors
-test -e ~/.vim/colors/monokai.vim || $fetcher ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim
-
-# General plugins
-plugins=(
-	ntpeters/vim-better-whitespace
-	tpope/vim-commentary
-	airblade/vim-gitgutter
-	machakann/vim-highlightedyank
-	sheerun/vim-polyglot
-	vim-scripts/ReplaceWithRegister
-	ngmy/vim-rubocop
-	justinmk/vim-sneak
-	kshenoy/vim-signature
-	tpope/vim-surround
-	tpope/vim-vinegar
-)
-
-# Regular Vim specific
-vim_plugins=(
-	vim-airline/vim-airline
-	ctrlpvim/ctrlp.vim
-	easymotion/vim-easymotion
-	adelarsq/vim-matchit
-)
-
-# Neovim specific
-neovim_plugins=(
-	norcalli/nvim-colorizer.lua
-	ojroques/nvim-hardline
-	phaazon/hop.nvim
-	nvim-lua/plenary.nvim
-	nvim-lua/popup.nvim
-	nvim-telescope/telescope.nvim
-)
-
-if ! command -v git >/dev/null 2>&1; then
-	echo 'You need git to retrieve the plugins.'
-	exit 1
-fi
-
-# Clone a plugin
-# param $1: base dir
-# param $2: plugin github
-clone_plugin () {
-	local plugdir=$(echo "$2" | cut -f2 -d/);
-	(cd "$1" || exit 1
-		if [[ ! -d $plugdir ]]; then
-			echo "Cloning:  $2"
-			git clone -q --depth=1 "https://github.com/$2"
-		else
-			(cd "$plugdir" || exit 1
-				echo "Updating: $2"
-				git pull -q
-			)
-		fi
-
-		vim -u NONE -c "helptags $plugdir/doc" -c q
-	)
-}
-
-# Iterate the list of plugins and clone.
-# $1 config location
-# $2 plugin list
-install_plugins () {
-	config="$1"
-	mkdir -p "$config"
-	shift
-	plugins=("$@")
-	for plug in "${plugins[@]}"; do
-		clone_plugin "$config" "$plug"
-	done
-}
-
 has_vim=$(command -v vim)
 has_neovim=$(command -v nvim)
-vim_config="$HOME/.vim/pack/packs/start"
-neovim_config="$HOME/.local/share/nvim/site/start"
 
-if [[ has_neovim || has_vim ]] ; then
+if [[ has_neovim ]]; then
+	if ! command -v git >/dev/null 2>&1; then
+		echo 'You need git to retrieve Paq.'
+		exit 1
+	fi
+
+	paq_loc="$HOME/.local/share/nvim/site/pack/paqs/opt/paq-nvim"
+	if [[ ! -d "$paq_loc" ]]; then
+		echo 'Cloning Paq'
+		git clone -q https://github.com/savq/paq-nvim.git "$paq_loc"
+	else
+		echo 'Paq is already installed, run :PaqInstall or :PaqUpdate'
+	fi
+elif [[ has_vim ]]; then
+	# Vim theme
+	if command -v wget >/dev/null 2>&1;   then fetcher='wget -O'
+	elif command -v curl >/dev/null 2>&1; then fetcher='curl -o'
+	fi
+
+	mkdir -p ~/.vim/colors
+	test -e ~/.vim/colors/monokai.vim || $fetcher ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim
+
+	# General plugins
+	plugins=(
+		vim-airline/vim-airline
+		ntpeters/vim-better-whitespace
+		tpope/vim-commentary
+		ctrlpvim/ctrlp.vim
+		easymotion/vim-easymotion
+		airblade/vim-gitgutter
+		machakann/vim-highlightedyank
+		adelarsq/vim-matchit
+		sheerun/vim-polyglot
+		vim-scripts/ReplaceWithRegister
+		ngmy/vim-rubocop
+		justinmk/vim-sneak
+		kshenoy/vim-signature
+		tpope/vim-surround
+		tpope/vim-vinegar
+	)
+
+	if ! command -v git >/dev/null 2>&1; then
+		echo 'You need git to retrieve the plugins.'
+		exit 1
+	fi
+
+	# Clone a plugin
+	# param $1: base dir
+	# param $2: plugin github
+	clone_plugin () {
+		local plugdir=$(echo "$2" | cut -f2 -d/);
+		(cd "$1" || exit 1
+			if [[ ! -d $plugdir ]]; then
+				echo "Cloning:  $2"
+				git clone -q --depth=1 "https://github.com/$2"
+			else
+				(cd "$plugdir" || exit 1
+					echo "Updating: $2"
+					git pull -q
+				)
+			fi
+
+			vim -u NONE -c "helptags $plugdir/doc" -c q
+		)
+	}
+
+	# Iterate the list of plugins and clone.
+	# $1 config location
+	# $2 plugin list
+	install_plugins () {
+		config="$1"
+		mkdir -p "$config"
+		shift
+		plugins=("$@")
+		for plug in "${plugins[@]}"; do
+			clone_plugin "$config" "$plug"
+		done
+	}
+
+	vim_config="$HOME/.vim/pack/packs/start"
+	# Regular vim plugins
 	install_plugins "$vim_config" "${plugins[@]}"
 fi
-if [[ has_neovim ]]; then
-	# Neovim plugins
-	install_plugins "$neovim_config" "${neovim_plugins[@]}"
-elif [[ has_vim ]]; then
-	# Regular vim plugins
-	install_plugins "$vim_config" "${vim_plugins[@]}"
-fi
-
