640520406cdb3826ae3c99833d1673609918e666
Andrew Snyder <snyder13@vt.edu>
2025-01-06 09:36:41 -0500

    fix: Dynamically get analog sink.

--- sway/.config/sway/switch-sink.sh
+++ sway/.config/sway/switch-sink.sh
@@ -1,24 +1,28 @@
 #! /usr/bin/env bash
 
-sinks=(
-	alsa_output.usb-Kingston_HyperX_7.1_Audio_00000000-00.analog-stereo
-	alsa_output.pci-0000_0b_00.4.analog-stereo
-)
-current_sink=$(pactl get-default-sink)
-message=
-case $current_sink in
-	"${sinks[0]}")
-		pactl set-default-sink "${sinks[1]}"
-		message="Output set to ${sinks[1]}"
-		;;
-	"${sinks[1]}")
-		pactl set-default-sink "${sinks[0]}"
-		message="Output set to ${sinks[0]}"
-		;;
-	*)
-		message="Unknown current sink: $current_sink"
-		;;
-esac
+headset=$(pactl list sinks | awk '/Name: alsa_output.pci.+analog-stereo/ {print $2}')
+
+if [[ ${#headset[@]} -gt 1 ]]; then
+	message="More than one analog-stereo sink"
+else
+	sinks=(alsa_output.usb-Kingston_HyperX_7.1_Audio_00000000-00.analog-stereo)
+	sinks+=("${headset[@]}")
+	current_sink=$(pactl get-default-sink)
+	message=
+	case $current_sink in
+		"${sinks[0]}")
+			pactl set-default-sink "${sinks[1]}"
+			message="Output set to ${sinks[1]}"
+			;;
+		"${sinks[1]}")
+			pactl set-default-sink "${sinks[0]}"
+			message="Output set to ${sinks[0]}"
+			;;
+		*)
+			message="Unknown current sink: $current_sink"
+			;;
+	esac
+fi
 
 notify-send \
 	--app-name sway \
